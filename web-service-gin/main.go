983bc776e8cb96a237582f17db33b0ed4f65c417
Xavier Ruiz <the.xavierruiz@gmail.com>
2024-12-28 13:39:18 -0600

    Added game data details and image icon to API

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -6,20 +6,48 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-// Data structures for api request responses
-
+// Models
 //{"response":{"steamid":"76561198305662842","success":1}}
-type SteamUser struct {
+type UserData struct {
 	Response struct {
 		Steamid string `json:"steamid"`
 		Success int `json:"success"`
 		Username string `json:"username"`
 	} `json:"response"`
+}
+
+type OwnedGame struct {
+	AppId int `json:"appid"`
+	GameName string `json:"name"`
+	Playtime int `json:"playtime_forever"` // Total # of minutes played
+	ImageIcon string `json:"img_icon_url"`
+	ImageLogo string `json:"img_logo_url"`
+}
+
+type GameData struct {
+	Response struct {
+		GameCount int `json:"game_count"`
+		OwnedGames []OwnedGame `json:"games"`
+	} `json:"response"`
+}
+
+
+type Data struct {
+	Username string `json:"username"`
+	SteamId string `json:"steamid"`
+	GameCount int `json:"game_count"`
+	Games []OwnedGame `json:"games"`
+}
+
+type Playtime struct {
+	TotalPlaytime int `json:"total_playtime"` // in minutes
+	MostPlaytime OwnedGame `json:"most_played_game"`
 }
 
 
@@ -28,12 +56,12 @@ func main() {
 	loadEnv()
 	router := gin.Default()
 	router.GET("/health", health)
-	router.GET("/getSteamUser", getSteamUser)
+	router.GET("/getData", getData)
 
 	router.Run() // default to 8080
 }
 
-
+// Utils
 func loadEnv() {
 	// Load environment variables
 	err := godotenv.Load(".env")
@@ -43,40 +71,85 @@ func loadEnv() {
 }
 
 
-// Business Logic
-func health(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "healthy")
-}
-
-
-func getSteamUser(c *gin.Context) {
+func httpGetRequest(url string) []byte {
 	steam_api_key := os.Getenv("STEAM_API_KEY")
-	username := "Kodiris"
-	url := "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" + steam_api_key + "&vanityurl=" + username
+	url = strings.Replace(url, "<API_KEY>", steam_api_key, -1)
+	println("URL: " + url)
 
 	// Make request
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
 
-	// Unmarshal response to struct
-	var steamUser SteamUser
-	json.Unmarshal(body, &steamUser)
-	steamUser.Response.Username = username
-	
+	return body
 
-	c.IndentedJSON(http.StatusOK, steamUser)
 }
-// func getAlbums(c *gin.Context) {
-	// c.IndentedJSON(http.StatusOK, albums)
+
+
+// Business Logic
+func health(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "healthy")
+}
+
+func getData(c *gin.Context) {
+	username := c.Query("username")
+
+	userData := getSteamUser(username)
+	gameData := getGameData(userData.Response.Steamid)
+
+	data := Data{
+		Username: userData.Response.Username, 
+		SteamId: userData.Response.Steamid,
+		GameCount: gameData.Response.GameCount,
+		Games: gameData.Response.OwnedGames,
+	}
+
+	c.IndentedJSON(http.StatusOK, data)
+}
+
+func getSteamUser(username string) UserData {
+	url := "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=<API_KEY>&vanityurl=" + username
+
+	body := httpGetRequest(url)
+	// Unmarshal response to struct
+	var userData UserData
+	json.Unmarshal(body, &userData)
+	userData.Response.Username = username
+	
+	return userData
+}
+
+func (game *GameData) Modify(index int, img_icon_url string) {
+  game.Response.OwnedGames[index].ImageIcon = img_icon_url
+}
+
+func getGameData(steamId string) GameData {
+	url := "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=<API_KEY>&steamid=" + steamId + "&format=json&include_appinfo=true"
+	var gameData GameData
+	body := httpGetRequest(url)
+	json.Unmarshal(body, &gameData)
+
+		
+	for indx, i := range gameData.Response.OwnedGames {
+		println(i.ImageIcon)
+		urlString := "http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg"
+		fullUrl := strings.Replace(urlString, "{appid}", fmt.Sprint(i.AppId), 1)
+		fullUrl = strings.Replace(fullUrl, "{hash}", i.ImageIcon, 1)
+		gameData.Modify(indx, fullUrl)
+	}
+	return gameData
+}
+
+// func getPlaytime() int {
+// 
 // }
- 
+
